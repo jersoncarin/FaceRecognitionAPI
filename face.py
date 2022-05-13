a9bdac3b0dc1be28c2e5a0fa6e70bfe8c7ba4d13
Julev Ajeto <jersoncarin25@gmail.com>
2022-05-13 21:24:26 +0800

    Update face.py

--- face.py
+++ face.py
@@ -34,25 +34,27 @@ def detect_face_id():
     # Get the list of images encoded
     encoded_images_list = encode_images(images)
 
-    if len(encoded_images_list) < 1:
-        return None
+    print(encoded_images_list)
 
-    # Check if the source id is fill and source image
-    if source is not None:
-        try:
-            faces_frame_location = face_recognition.face_locations(source)
-            encoded_face_frame = face_recognition.face_encodings(source, faces_frame_location)
+    # if len(encoded_images_list) < 1:
+    #     return None
 
-            for face in encoded_face_frame:
-                matches = face_recognition.compare_faces(encoded_images_list, face)
-                face_distance = face_recognition.face_distance(encoded_images_list, face)
-                # Find the lowest index so that is the best match
-                bestMatchIndex = np.argmin(face_distance)
-                # Check if the index is the best match of the face
+    # # Check if the source id is fill and source image
+    # if source is not None:
+    #     try:
+    #         faces_frame_location = face_recognition.face_locations(source)
+    #         encoded_face_frame = face_recognition.face_encodings(source, faces_frame_location)
+
+    #         for face in encoded_face_frame:
+    #             matches = face_recognition.compare_faces(encoded_images_list, face)
+    #             face_distance = face_recognition.face_distance(encoded_images_list, face)
+    #             # Find the lowest index so that is the best match
+    #             bestMatchIndex = np.argmin(face_distance)
+    #             # Check if the index is the best match of the face
                 
-                if matches[bestMatchIndex] and image_ids[bestMatchIndex]:
-                    return image_ids[bestMatchIndex]
-        except IndexError:
-            return None
+    #             if matches[bestMatchIndex] and image_ids[bestMatchIndex]:
+    #                 return image_ids[bestMatchIndex]
+    #     except IndexError:
+    #         return None
     return None
                 
